99581126599c72368b00041106be7b27ae5bc8a5
daniiel-fernando <98991046+daniiel-fernando@users.noreply.github.com>
2023-08-25 16:40:04 -0300

    atualização
    os links agora estão funcionando porem falta corrigir os nav do sobre e portfolios

--- Main/main.js
+++ Main/main.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
       }, 500);
   });
 });
+    function redirectToPage(page) {
+      window.location.href = page;
+    }
+    document.addEventListener("DOMContentLoaded", function () {
+      // Selecione todos os links da barra de navegação
+      var links = document.querySelectorAll(".navbar-nav a.nav-link");
+  
+      // Adicione um ouvinte de evento de clique a cada link
+      links.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+          // Verifique se o link possui um atributo href válido
+          if (this.getAttribute("href") && this.getAttribute("href") !== "#") {
+            // Redirecione para a URL especificada no atributo href
+            window.location.href = this.getAttribute("href");
+          }
+        });
+      });
+    });
+  
